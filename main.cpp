9a7e97d48c6b0fe08dd0825167876545206947fa
Alex Lapachuk <alexlapachuk@AlexMacbook-Air.local>
2022-10-26 18:01:15 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int amountOfSymbols;
+    int lineOfSymbols;
+    char typeOfSymbol;
+    int a = 1;
+    cout << "Выберите количество символов" << endl;
+
+    cin >> amountOfSymbols;
+
+    cout << "Введите тип символа" << endl;
+
+    cin >> typeOfSymbol;
+
+    cout << "Выберите положение линии" << endl <<
+         "1. Горизонтальная" << endl <<
+         "2. Вертикальная" << endl;
+
+    cin >> lineOfSymbols;
+
+    for (int i = 0; i < amountOfSymbols; ++i) {
+        switch (lineOfSymbols) {
+            case 1: {
+                cout << typeOfSymbol;
+                break;
+            }
+            case 2: {
+                cout << typeOfSymbol;
+                cout << "\n";
+                break;
+            }
+            default: {
+                cout << "Неправильный тип данных!" << endl;
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
